47d51675a6080c6ff3df7af1bdd64c56af6f895c
Jesse Hanson <jesse@mobilecart.io>
2017-01-05 20:03:06 -0500

    fix logic of curl request

--- Service/RecaptchaV2Service.php
+++ Service/RecaptchaV2Service.php
@@ -49,10 +49,10 @@ class RecaptchaV2Service
         rtrim($postStr, '&');
 
         $ch = curl_init();
-
-        curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_POST, count($params));
-        curl_setopt($ch,CURLOPT_POSTFIELDS, $postStr);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);// count($params));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postStr);
         $json = curl_exec($ch);
         curl_close($ch);
 
