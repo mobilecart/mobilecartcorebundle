564c6f779a25a9e3a9fae685537f2ebde3b53d87
Jesse Hanson <jesse@mobilecart.io>
2017-02-12 17:04:25 -0500

    product delete

--- EventListener/Product/ProductDelete.php
+++ EventListener/Product/ProductDelete.php
@@ -47,6 +47,51 @@ class ProductDelete
         $returnData = $this->getReturnData();
 
         $entity = $event->getEntity();
+
+        // remove category_product
+        $categoryProducts = $this->getEntityService()->findBy(EntityConstants::CATEGORY_PRODUCT, [
+            'product' => $entity->getId(),
+        ]);
+
+        if ($categoryProducts) {
+            foreach($categoryProducts as $categoryProduct) {
+                $this->getEntityService()->remove($categoryProduct, EntityConstants::CATEGORY_PRODUCT);
+            }
+        }
+
+        // remove product images
+        $productImages = $this->getEntityService()->findBy(EntityConstants::PRODUCT_IMAGE, [
+            'parent' => $entity->getId(),
+        ]);
+
+        if ($productImages) {
+            foreach($productImages as $productImage) {
+                $this->getEntityService()->remove($productImage, EntityConstants::PRODUCT_IMAGE);
+            }
+        }
+
+        // remove product configs
+        $productConfigs = $this->getEntityService()->findBy(EntityConstants::PRODUCT_CONFIG, [
+            'product' => $entity->getId(),
+        ]);
+
+        if ($productConfigs) {
+            foreach($productConfigs as $productConfig) {
+                $this->getEntityService()->remove($productConfig, EntityConstants::PRODUCT_CONFIG);
+            }
+        }
+
+        // remove product configs
+        $productConfigs = $this->getEntityService()->findBy(EntityConstants::PRODUCT_CONFIG, [
+            'child_product' => $entity->getId(),
+        ]);
+
+        if ($productConfigs) {
+            foreach($productConfigs as $productConfig) {
+                $this->getEntityService()->remove($productConfig, EntityConstants::PRODUCT_CONFIG);
+            }
+        }
+
         $this->getEntityService()->remove($entity, EntityConstants::PRODUCT);
 
         if ($entity && $event->getRequest()->getSession()) {
